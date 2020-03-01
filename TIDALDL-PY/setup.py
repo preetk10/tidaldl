dac9afceda63667d8a4ab7687205547e0a4d4c74
H. Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2020-03-01 12:52:59 +0300

    Update setup.py

--- TIDALDL-PY/setup.py
+++ TIDALDL-PY/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name = 'tidal-dl',
-    version="2020.2.14.0",
+    version="2020.2.28.1",
     license="Apache2",
-    description = "Tidal Music Download.",
+    description = "Yusuf Usta Version",
 
     author = 'YaronH',
     author_email = "yaronhuang@qq.com",
@@ -11,6 +11,6 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires=["aigpy>=2020.2.14.0", "requests", "ffmpeg", "pycryptodome", "pydub", ],
+    install_requires=["aigpy>=2020.2.28.0", "requests", "ffmpeg", "pycryptodome", "pydub", ],
     entry_points={'console_scripts': [ 'tidal-dl = tidal_dl:main', ]}
 )
